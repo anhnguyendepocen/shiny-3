c97496bfbfa5bb91eb4ae98712687baf49e885ef
Dwight Townsend <townsenddw@gmail.com>
2018-09-04 18:01:45 -0700

    moving bootstrap.sh to shiny-base

---
+++ images/shiny-base/bootstrap.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This sed madness searches all shiny-app dirs and consolidates dependencies
+#  into a single install.R file
+APP_DIR=../../shiny-apps
+echo "install.packages(c(" > install.R
+grep -rh library $APP_DIR/* \
+  | sed "s/^[ \t]*//" \
+  | sed s/library//g \
+  | sed s/\"//g \
+  | sort -u \
+  | sed s/\(/'"'/g \
+  | sed s/\)/'",'/g \
+  | sed '$s/,$//' >> install.R
+echo "), dependencies=TRUE)" >> install.R
+
+# devtools installs
+echo 'library("devtools")' >> install.R
+grep -rho devtools::install.* $APP_DIR/* \
+  | sort -u >> install.R
